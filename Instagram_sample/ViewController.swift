76824a32dca6fbc13cc682259842ae3dd2e07f73
Koichi Tanaka <kouichi.tanaka@gmail.com>
2016-02-16 14:58:20 -0800

    Initial Commit

---
+++ Instagram_sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram_sample
+//
+//  Created by koichi tanaka on 2/16/16.
+//  Copyright © 2016 koichi tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
